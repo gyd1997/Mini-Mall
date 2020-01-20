a3b8e965a36100474889ad5fd22969bb5463a5bf
gyd1997 <gyd0008@163.com>
2020-01-21 01:02:55 +0800

    请求商品数据

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,14 +1,32 @@
-import {getMultiData} from '../../service/home.js'
+import {
+  getMultiData,
+  getGoodsData
+} from '../../service/home.js'
 Page({
 
   data: {
     banners: [],
     recommends: [],
-    titles: ['新款', '流行', '精选']
+    titles: ['流行', '新款', '精选'],
+    goods: {
+      'pop': {page: 0, list: []},
+      'new': {page: 0, list: []},
+      'sell': {page: 0, list: []}
+    }
   },
 
   onLoad: function (options) {
-    // 请求轮播图及推荐数据
+    // 1.请求轮播图及推荐数据
+    this._getMultiData()
+
+    // 2.请求商品数据
+    this._getGoodsData('pop')
+    this._getGoodsData('new')
+    // this._getGoodsData('sell')
+  },
+
+  //------------- 请求轮播图及推荐数据----------------
+  _getMultiData() {
     getMultiData().then(res => {
       const banners = res.data.data.banner.list
       const recommends = res.data.data.recommend.list
@@ -19,6 +37,29 @@ Page({
     })
   },
 
+  // ------------ 请求商品列表数据-------------------
+  _getGoodsData(type) {
+    // 1.获取页码
+    const page = this.data.goods[type].page + 1
+    // 2.请求数据
+    getGoodsData(type, page).then(res => {
+      // 2.1 取出数据
+      const list = res.data.data.list
+      // 2.2 将数据设置到对应 type 的 list 中
+      const oldList = this.data.goods[type].list
+      oldList.push(...list)
+      // 2.3 将数据设置到 data 的 goods中
+      const typeKey = `goods.${type}.list`
+      const pageKey = `goods.${type}.page`
+      this.setData({
+        [typeKey]: oldList,
+        [pageKey]: page
+      })
+
+    })
+  },
+
+  //------------- 处理 tab 点击----------------------
   handleTabClick(e) {
     const index = e.detail.index
     console.log(index)
